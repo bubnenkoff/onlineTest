0ae22076ad347a161869969b20e073f6f2247ea7
Dmitry <bubnenkoff@gmail.com>
2015-11-17 11:59:02 +0300

    multiple choice still not work

--- js/questions.js
+++ js/questions.js
@@ -32,7 +32,7 @@ app.controller("TestQuestions", function($scope)
 										 "isSelected" : 0
 									}
 								 ],
-						"allowMultipleChoice" : true,
+						"MaxAllowedChoice" : 1,
 						"isEnabled" : true,
 						"NowSelected" : 0
 				},
@@ -57,7 +57,7 @@ app.controller("TestQuestions", function($scope)
 																											"isSelected" : 0
 																										}
 																									  ],
-					"allowMultipleChoice" : true,
+					"MaxAllowedChoice" : 2,
 					"isEnabled" : true,
 					"NowSelected" : 0
 				},
@@ -67,95 +67,38 @@ app.controller("TestQuestions", function($scope)
 																			{"id": 1, "answer" : "Dima", "isSelected" : 0}, 
 																			{"id": 2, "answer": "Roma", "isSelected" : 0}, 
 																			{"id": 3, "answer": "Masha", "isSelected" : 0}],
-					"allowMultipleChoice" : false,
+					"MaxAllowedChoice" : 1,
 					"isEnabled" : true,
-					"NowSelected" : 0
+					"NowSelected" : 0,
+					"foo":"TESTTEST"
 				},
 				{
 					"id": 4, "question": "Where do you live?", "answers": ["Moscow", "London", "Minsk"],
 					"isEnabled" : true,
+					"MaxAllowedChoice" : 2,
 					"NowSelected" : 0
 				}
 		],
 
 		$scope.maxCheckedValues = 1,
 
-		 //console.log($scope.questions[1].question);
-		 // console.log($scope.questions[1].question);
-
-		// $scope.$watchCollection("questions", function(newVal, oldVal)
-		// 	{
-				
-
-				// if ($scope.maxCheckedValues >1)
-				//nowSelected = 0;
-		
-				// for (var key in $scope.questions)
-				// {
-				// 	//console.log($scope.questions[key]);
-				// 	// if selected more than maxCheckedValues elements we should set all isSelected elements to 0
-				// 	if ($scope.questions[key].allowMultipleChoice == true)
-				// 		// for(var i in $scope.questions[key].question)
-				// 		// {
-				// 		// 	console.log(question[i]);
-				// 		// }
-				// 		// angular.forEach(i, key)
-				// 		// {
-				// 		// 	console.log(i);
-				// 		// }
-				// }
-
-				numberOfSelected = 0;
-				angular.forEach($scope.questions, function(question)
+		$scope.$watch("questions", function(newVal, oldVal)
+		{
+			angular.forEach($scope.questions, function(question)
 				{
 					angular.forEach(question.answers, function(answer)
 					{
-						// console.log(answer.isSelected);
-							if (answer.isSelected == 2) //if double click
-							{
-								answer.isSelected == 0 // set back to zero	
-								console.log(answer.isSelected);
-							}
-							
-						 if(answer.isSelected == 1)
-						 {
-						 	numberOfSelected++
-						 	if (numberOfSelected >= $scope.maxCheckedValues)
-						 		{
-						 			angular.forEach(question.answers, function(answer)
-									{
-										console.log(numberOfSelected);
-										/*
-										$scope.$watchCollection(answer.isSelected, function(newVal, oldVal)
-										{
-											if(newVal >= 2)
-											{
-												answer.isSelected = 0;
-												console.log(answer.isSelected);
-												console.log(newVal);
-											}
-										});
-										*/
-										//console.log(answer.isSelected);
-									});
-						 		}
-						 	// console.log(numberOfSelected);
-						 }
-						 ///console.log(numberOfSelected);
+
+						if (answer.isSelected >= 2) // if we move back to zero we should decrement NowSelected
+						{
+							 answer.isSelected = 0;
+						}
+
 					});
 				});
-				//console.log(numberOfSelected);
-				
 
-				// angular.forEach($scope.questions, function (question) {
-				//     angular.forEach(question.answers, function (answer) {
-				//         console.log(answer.isSelected);
-				//     });
-				// });
-				 
-			//} // uncomment me!
+		}, true);	
 
-			//);
 	
 	});
 
