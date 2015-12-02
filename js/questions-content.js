fb0e1bf7ba0b205907a79cde2e8f6f0a5f5b359d
Dmitry <bubnenkoff@gmail.com>
2015-12-02 14:21:07 +0300

    footer added loog ok

--- js/questions-content.js
+++ js/questions-content.js
@@ -5,9 +5,111 @@ app.controller("TestQuestions", function($scope, $http)
   		$scope.minArea = 10;
     	$scope.maxArea = 90;
 
-		$scope.questions = $http.get('questions-content.js').success(function(response) {
-        return response.data;
-    });,
+		$scope.questions =
+		[
+				{
+					"id": 1, 
+					"question": "Каков уровень Вашей организации?", 
+						"answers": [
+									{
+										"id" : 1,
+										"answer":	"Федеральные органы исполнительной власти",
+										"isSelected" : 0
+									}, 
+									{	"id" : 2,
+										 "answer": "Региональные органы исполнительной власти",
+										 "isSelected" : 0
+									}, 
+									{	 "id" : 3,
+										 "answer": "Местные административные органы управления",
+										 "isSelected" : 0
+									}, 
+									{	 "id" : 4,
+										 "answer": "Некоммерческие организации",
+										"isSelected" : 0
+									}, 
+									{	 "id" : 5,
+										 "answer": "Частное лицо",
+										 "isSelected" : 0
+									}, 
+									{	 "id" : 6,
+										 "answer": "Другое",
+										 "isSelected" : 0
+									}
+								 ],
+						"MaxAllowedChoice" : 1,
+						"isEnabled" : true,
+						"NowSelected" : 0
+				},
+
+				{
+					"id": 2, 
+					"question": "Какая главная область использования Вами данных ДЗЗ?", "answers": [
+																										{
+																											"id" : 1,
+																											"answer": "Сельское хозяйство",
+																											"subanswers":
+																													[
+																														{
+																															"id":1,
+																															"subanswer": "Растеневодство",
+																															"subisSelected" : 0
+																														},
+																														{
+																															"id":2,
+																															"subanswer": "Животноводство",
+																															"subisSelected" : 0
+																														},
+																													],
+																											"isSelected" : 0
+																										}, 
+																										{
+																											"id" : 2,
+																											"answer": "Лесное хозяйство",
+																											"subanswers":
+																													[
+																														{
+																															"id":1,
+																															"subanswer": "Вырубки",
+																															"subisSelected" : 0
+																														},
+																														{
+																															"id":2,
+																															"subanswer": "Состояние лесонасаждений",
+																															"subisSelected" : 0
+																														},
+																													],																											
+																											"isSelected" : 0
+																										},
+
+																										{
+																											"id": 3,
+																											"answer": "Землепользование",
+																											"isSelected" : 0
+																										}
+																									  ],
+					"MaxAllowedChoice" : 3,
+					"isEnabled" : true,
+					"NowSelected" : 0
+				},
+
+				{
+					"id": 3, "question": "What is your name?", "answers": [
+																			{"id": 1, "answer" : "Dima", "isSelected" : 0}, 
+																			{"id": 2, "answer": "Roma", "isSelected" : 0}, 
+																			{"id": 3, "answer": "Masha", "isSelected" : 0}],
+					"MaxAllowedChoice" : 2,
+					"isEnabled" : true,
+					"NowSelected" : 0,
+					"foo":"TESTTEST"
+				},
+				{
+					"id": 4, "question": "Where do you live?", "answers": ["Moscow", "London", "Minsk"],
+					"isEnabled" : true,
+					"MaxAllowedChoice" : 2,
+					"NowSelected" : 0
+				}
+		],
 
 
 
